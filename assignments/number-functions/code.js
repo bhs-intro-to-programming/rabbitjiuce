16a6354eebfbe6e5005885b78ba495dfd1c814aa
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-09-07 17:24:26 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -3,4 +3,6 @@ const subtract = (a,v) => a - v
 const multiply = (a,v) => a * v 
 const divide = (a,v) => a / v
 const mod = (a,v) => a%v 
-const averageOfTwo = (a,v) => a && v / av
+const averageOfTwo = (a,v) => a + v 
+
+const distance = (a,v) => a - v
