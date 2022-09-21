ee16ead3d4626f1a23d68ea499cd9167092d1c3e
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-09-21 15:18:52 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -71,3 +71,4 @@ drawFilledCircle(250, 300, 50, 'blue')
 drawRect(250, 50, 300, 100, 'red');
 drawCircle(55, 550, 30, 'blue') 
 drawCircle(430, 450, 30, 'blue')
+drawLine(x1, y1, x2, y2, color, lineWidth)
