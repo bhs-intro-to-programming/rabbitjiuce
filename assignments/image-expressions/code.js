397b7d7378c803b8695c58e64f38a9067eaeb53c
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-09-21 20:54:23 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -72,6 +72,4 @@ drawRect(125, 50, 250, 100, 'red');
 drawCircle(55, 550, 30, 'blue') 
 drawCircle(430, 450, 30, 'blue')
 drawLine(55, 550, 430, 450, 'black')
-drawTriangle(245, 55
-, 370, 145
-, 130, 145) 
+drawTriangle(245, 55, 370, 145, 130, 145, 'blue') 
