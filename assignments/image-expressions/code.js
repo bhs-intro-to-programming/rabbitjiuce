47b9447e0465aa89655dabd504eedd51145a7986
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-09-27 15:00:58 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -78,5 +78,5 @@ drawFilledCircle(height/2, width/2, 50, 'blue')
 //it doesn't really look centered though???? uhhh maybe it's just my screen 
 drawRect(width/4, 50, (width/4) * 3, 100, 'red')
 // it still isn't looking centered ;-; 
-drawTriangle(width/2, 55, (width/3) - 5, 95, width/4, 95) 
+drawTriangle(width/2, 55, (width/3) - 5, 95, width/4, 95, 'blue') 
 
