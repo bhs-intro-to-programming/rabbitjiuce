1c7e3b8fb2907cc422ebaf54f7cadb51ade916a1
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-09-27 15:00:46 -0700

    Updating assignments/image-expressions/code.js

--- assignments/image-expressions/code.js
+++ assignments/image-expressions/code.js
@@ -80,4 +80,3 @@ drawRect(width/4, 50, (width/4) * 3, 100, 'red')
 // it still isn't looking centered ;-; 
 drawTriangle(width/2, 55, (width/3) - 5, 95, width/4, 95) 
 
-drawTriangle(245, 55, width/2, 145, 130, 145, 'blue') 
