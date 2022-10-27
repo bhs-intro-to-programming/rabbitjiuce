ba9604c85e8a67f56d1181bf48ddc569285cf1e0
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-10-27 14:51:41 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,9 +1,9 @@
 // i'm writing in shorthand because writing the full thing all the time is annoying 
 const findFnord = (s) => s.indexOf ('fnord')
 
-const stringContains = (s,s2) => s.indexOf ('s, s2') 
-
-
+const stringContains = (s, s2) => {
+  return s.indexOf ('s2')
+} 
 // ayo look at ur own screen
 
 
