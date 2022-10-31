10cb284ae7bd776c75f89d111756b2823443a30f
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-10-31 15:10:24 -0700

    Updating assessments/2022-10-31-strings/code.js

--- assessments/2022-10-31-strings/code.js
+++ assessments/2022-10-31-strings/code.js
@@ -37,7 +37,7 @@ const firstAndLast = (s) => {
 let f = 'fghjkkla'  
 
 const swapFrontAndBack = (s) => {
-  return s[s.length/2] + s.substring(0, s.length/2)
+  return s.substring(s.length/2) + s.substring(0, s.length/2)
 }
 
 
