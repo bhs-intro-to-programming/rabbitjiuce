c31b49a6eebe322930189faf08e3dedaca921f06
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-11-09 15:07:41 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -10,8 +10,11 @@ const emptyObject = () => {
 }
 
 const distance = (point1, point2) => {
-  return ((((point1:x) - (point2:x))**2) + (((point1:y) - (point2:y))**2))Math.sqrt
+  return ((((point2:x) - (point1:x))**2) + (((point2:y) - (point1:y))**2))Math.sqrt
 }
+
+
+
 
 
 //indexOf 
@@ -19,5 +22,5 @@ const distance = (point1, point2) => {
 
 
 
-//square root of xaxis distance + yaxis distance 
+//square root of xaxis distance squared + yaxis distance squared
 
