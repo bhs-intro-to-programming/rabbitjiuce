5763ce9736b91fb0e90dcd96558a0bed74a89169
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-11-10 15:25:29 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -17,6 +17,14 @@ const midpoint =(p1, p2) => {
   return {'x' : (p1.x + p2.x)/2, 'y' : (p1.y + p2.y)/2} 
   }
 
+const summarizeBooks = ([myArray]) => {
+  return myArray.title + myArray.pages
+}
+
+
+
+
+
 
 
 
