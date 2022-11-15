e98833d29e3bf32901fbeafb8b71b22933e96f10
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-11-15 15:06:13 -0800

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -37,8 +37,8 @@ const pigLatin = (word) => word.substring(word.search(/[aeiou]/)) + word.substri
 
 const advancedPigLatin = (word) => {
   if (word.search(/[aeiou]/) = 0 ) {
-    word + 'way'
+    //word + 'way'
 } else {
-    word.substring(word.search(/[aeiou]/)) + word.substring(0, word.search(/[aeiou]/)) + 'ay'
+   // word.substring(word.search(/[aeiou]/)) + word.substring(0, word.search(/[aeiou]/)) + 'ay'
 }
 }
