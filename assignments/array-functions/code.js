c730aebedcb729638561a6178a3724fc22cec99b
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-11-29 15:30:55 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -35,14 +35,3 @@ const anyOverOneHundred = (array) => {
   return false 
 }
 
-const pyramid = (array) => {
- const idk = [];
-  for (let i = 1; i<array.length; i++) {
-    for (let j = 1; i<array.length; j++) {
-       idk.push(i); 
-    } 
-  } 
-return idk; 
-};
-
-
