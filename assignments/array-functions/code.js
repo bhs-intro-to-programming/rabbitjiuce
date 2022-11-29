5f5cdd59fadcfe5a95b1216e1572d6979b193d3a
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-11-29 14:56:35 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 const countTens = (ns) => {
   let count = 0 
-  for (a = 0; a < ns.length; i++) { 
-    if (ns[i] === 1) { 
+  for (a = 0; a < ns.length; a++) { 
+    if (ns[a] === 1) { 
       count++
     } 
   }
