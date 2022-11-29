6a271bd2cb5043345c77b126f2c149b175abb851
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-11-29 15:06:44 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -11,7 +11,7 @@ const countTens = (array) => {
 const sum = (array) => {
   let total = 0 
   for (let i = 0; i < array.length; i++) {
-    total + array[i] 
+    total = total + array[i] 
   }
   return total 
 }
