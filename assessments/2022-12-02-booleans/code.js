8a9689d6a5393d7464e0bb2d01d69b41028fa7a8
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-02 14:50:44 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -5,3 +5,4 @@ const isVenomous = (striped, blueHead) => striped || !blueHead
 const okaySpeed = (speedLimit, speed) => (speed < (speedLimit+11)) && (speed > (speedLimit - 11))
 const twiceAsExpensive = (price, otherPrice) => (2*price) > otherPrice 
 const winningRecord = (wins, losses, ties) => (wins/2 + losses/2 + ties/2) < wins
+const isMagicNumber = (number) => number === (42 || 17) 
