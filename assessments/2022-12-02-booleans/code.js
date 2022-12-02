03af8599d9ee65b1b578d0a4dbb06976b0eb54e4
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-02 14:43:17 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,3 +1,4 @@
 const needHeavyCoat = (out, cold) => out && cold 
 const needSunscreen = (beach, skiing) => beach || skiing 
 const needMittens = (out, warm) => out && !warm 
+const isVenomous = (striped, blueHead) => striped || !blueHead 
