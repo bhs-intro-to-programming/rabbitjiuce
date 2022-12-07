b61cb4952e6fe6be0f2cec6df4958aa881879fae
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-07 15:19:07 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -123,7 +123,8 @@ const threewayClassify = (aValue) => {
 const sumOfSquares = (num) => {
   let squares = 0
   for (let i=0; i<num; i++) {
-    let squares = (num-i**2) + squares
+    let squares = (num-i**2) + squares {
+    }
   }
   return squares
 }
