7c637ff10f263b6b9e6d7f0ae42d65010aa012b1
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-08 14:42:02 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -1,3 +1,13 @@
 const yesIfEven = (num) => {
   return num%2 === 0 
 }
+
+const countXs = (s) => {
+  let count = 0; 
+  for (let i=0; i<s.length; i++) {
+    if (s[i] === 'x') {
+      count++ 
+    }
+  }
+  return count 
+}
