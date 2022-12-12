859fb2f2ccf54e20396205084196cfbbddef1f0a
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-12 15:03:27 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -6,7 +6,7 @@ const makeBoard = () => {
   return [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 }
 
-const makeMove(s, num1, num2) => {
+const makeMove = (s, num1, num2) => {
   return //what does this mean i literally don't undestand this at all 
 }
 
