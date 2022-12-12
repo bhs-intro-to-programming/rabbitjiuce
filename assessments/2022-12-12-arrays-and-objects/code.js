0c2dc68f8346a7bc0847b0e836caf775e777f2ee
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-12 15:06:56 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -8,5 +8,14 @@ const makeBoard = () => {
 
 const makeMove = (s, num1, num2) => {
   return //what does this mean i literally don't undestand this at all 
+  //what's a mark 
 }
 
+const placeMark = (array, obj) => {
+  return makeBoard 
+  //w hat does this meeeean 
+}
+
+const allTheSame([a, b, c] => {
+  return a===b===c
+}
