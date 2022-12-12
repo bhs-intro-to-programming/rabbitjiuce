336ecc0980863d0791d6081988aba36ecc62000b
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-12 15:03:20 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -5,3 +5,8 @@ const makeRow = () => {
 const makeBoard = () => {
   return [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 }
+
+const makeMove(s, num1, num2) => {
+  return //what does this mean i literally don't undestand this at all 
+}
+
