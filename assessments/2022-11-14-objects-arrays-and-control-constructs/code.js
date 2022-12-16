9ab07e5e648ef7ab779309a226129a84bae1d2e1
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-16 10:51:35 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -1,7 +1,7 @@
 const area = (obj) => obj.width ** obj.height 
 const higherPaid = (obj, obj2) => {
   if obj.salary > obj2.salary {
-    return obj
+    return obj;
   } else {
     return obj2.salary
   }
