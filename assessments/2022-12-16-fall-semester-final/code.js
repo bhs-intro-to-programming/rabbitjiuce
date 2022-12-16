ac7728ac23fa6b43f1291786c7b1cec0b54f15a2
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-16 11:29:06 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -127,7 +127,7 @@ const presentsBudget = (nFriend, avPrice) => nFriend*avPrice
 const wrappingCombos = (numPaper, numRibbons, numBows) => numPaper * numRibbons * numBows 
 
 const biggestNumber = (digits) => {
-  let nines = 0
+  let nines = nines
   for (let i=0; i<digits; i++) {
     nines = nines + '9'
   }
