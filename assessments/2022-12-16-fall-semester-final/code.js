0f4dbff6477781e6dd268fe8b13129fb8fa3ff14
rabbitjiuce <112589062+rabbitjiuce@users.noreply.github.com>
2022-12-16 11:50:53 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -157,3 +157,6 @@ const fireAlarm = (pulled, smoke, drill) => pulled || smoke || drill
 
 const canBePresident = (age, naturalBorn, lived) => (Math.floor(age))>34 && naturalBorn && (Math.floor(lived))>13 
 
+const needSunScreen = (beach, ski) => beach || skiing 
+
+const canGoToProm = (senior, inviteSen, promEx) => (senior || inviteSen) && !promEx
